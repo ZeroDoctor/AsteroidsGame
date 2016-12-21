e9da2c2f4ef0ab9cde1a812c88c795d74c05fdd0
Spider-top <Spider-top@10.0.0.207>
2016-12-21 13:18:47 -0600

    Daniel, check the end of the player class

--- src/com/zerulus/entity/Player.java
+++ src/com/zerulus/entity/Player.java
@@ -1,6 +1,43 @@
 package com.zerulus.entity;
 
+import java.awt.image.BufferedImage;
+
 public class Player 
 {
-	//now?
+	private BufferedImage[] flyingUp = {Sprite.getSprite(0, 2), Sprite.getSprite(1,  2), Sprite.getSprite(2, 2)};
+	private BufferedImage[] staying = {Sprite.getSprite(0, 0)};
+	private BufferedImage[] startingUp = {Sprite.getSprite(1, 0), Sprite.getSprite(2, 0), Sprite.getSprite(0, 1), Sprite.getSprite(1, 1), Sprite.getSprite(2, 1)}
+	
+	private Animation flyUp = new Animation(flyingUp, 10);
+	private Animation stay = new Animation(staying, 10);
+	private Animation startUP = new Animation(startingUp, 10);
+	
+	
 }
+
+/* STILL NEEDS
+ This is the actual animation
+private Animation animation = standing;
+
+In your update or tick method you will have:
+
+animation.update();
+
+In your render or draw method you will have:
+
+g.drawImage(animation.getSprite(), x, y, null);
+
+Lets say you have implemented MouseListener
+
+public void mousePressed(MouseEvent e) {
+    animation = walkLeft;
+    animation.start();
+}
+
+public void mouseReleased(MouseEvent e) {
+    animation.stop();
+    animation.reset();
+    animation = standing;
+}
+ */
+*/
