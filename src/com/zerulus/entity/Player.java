1b9aa4e36e2911e859a5672bce3431bcfce34155
Zerulus <danielrcastro10@gmail.com>
2016-12-24 11:36:46 -0600

    added images, movement, and rotation

--- src/com/zerulus/entity/Player.java
+++ src/com/zerulus/entity/Player.java
@@ -1,23 +1,141 @@
 package com.zerulus.entity;
 
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 
+import com.zerulus.main.GamePanel;
 import com.zerulus.util.Animation;
 import com.zerulus.util.Sprite;
+import com.zerulus.util.Vector2i;
 
 public class Player 
 {
-	private Sprite spr_ship = new Sprite("/Game/RocketShip.jpg");
+	private Sprite spr_ship = new Sprite("/Game/RocketShip.png");
 	
 	private BufferedImage[] flyingUp = {spr_ship.getSprite(0, 2), spr_ship.getSprite(1,  2), spr_ship.getSprite(2, 2)};
 	private BufferedImage[] staying = {spr_ship.getSprite(0, 0)};
 	private BufferedImage[] startingUp = {spr_ship.getSprite(1, 0), spr_ship.getSprite(2, 0), spr_ship.getSprite(0, 1), spr_ship.getSprite(1, 1), spr_ship.getSprite(2, 1)};
 	
+	private BufferedImage img_player;
+	
 	private Animation flyUp = new Animation(flyingUp, 10);
 	private Animation stay = new Animation(staying, 10);
 	private Animation startUP = new Animation(startingUp, 10);
 	
+	private Vector2i vec;
+	private int dx;
+	private int dy;
+	private int size = 20;
 	
+	public boolean up;
+	public boolean down;
+	public boolean left;
+	public boolean right;
+	
+	private int maxSpeed = 32;
+	private int acc = 3;
+	private int deacc = 1;
+	
+	private AffineTransformOp op;
+	private AffineTransform tx;
+	
+	private BufferedImage result;
+	
+	public Player() {
+		vec = new Vector2i(GamePanel.WIDTH / 2 - size / 2, GamePanel.HEIGHT / 2 - size / 2);
+		rotate(0);
+	}
+	
+	public void move() {
+		if(up) {
+			dy -= acc;
+			if(dy < -maxSpeed) {
+				dy = -maxSpeed;
+			}
+		} else {
+			if(dy < 0) {
+				dy += deacc;
+				if(dy > 0) {
+					dy = 0;
+				}
+			}
+		}
+		if (down) {
+			dy += acc;
+			if(dy > maxSpeed) {
+				dy = maxSpeed;
+			}
+		} else {
+			if(dy > 0) {
+				dy -= deacc;
+				if(dy < 0) {
+					dy = 0;
+				}
+			}
+		}
+		if(left) {
+			dx -= acc;
+			if(dx < -maxSpeed) {
+				dx = -maxSpeed;
+			}
+		} else {
+			if(dx < 0) {
+				dx += deacc;
+				if(dx > 0) {
+					dx = 0;
+				}
+			}
+		}
+		if(right) {
+			dx += acc;
+			if(dx > maxSpeed) {
+				dx = maxSpeed;
+			}
+		} else {
+			if(dx > 0) {
+				dx -= deacc;
+				if(dx < 0) {
+					dx = 0;
+				}
+			}
+		}
+	}
+	
+	public void rotate(double angle) {
+		double rotation = angle;
+		tx = AffineTransform.getRotateInstance(-rotation, size, size);
+		if(tx == null)
+			System.out.println("Nah");
+		
+		op = new AffineTransformOp(tx, AffineTransformOp.TYPE_BILINEAR);
+	}
+	
+	public void update(int mouseX, int mouseY) {
+		
+		
+		move();
+		
+		
+		rotate(Math.atan2(vec.x - mouseX, vec.y - mouseY));
+		
+		vec.x += dx;
+		vec.y += dy;
+
+		
+		img_player = stay.getSprite();
+		stay.start();
+	}
+	
+	public void render(Graphics2D g) {
+		if(img_player != null) {
+			g.drawImage(op.filter(img_player, null), vec.x, vec.y, null);
+		} else {
+			g.drawImage(img_player, vec.x, vec.y, size, size, null);
+		}
+		
+	}
 }
 
 /* STILL NEEDS
