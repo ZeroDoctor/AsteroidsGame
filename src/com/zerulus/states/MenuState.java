7b706fd3899ff2b1cd1e02ca8eea057cdb1ada1b
Zerulus <danielrcastro10@gmail.com>
2016-12-26 10:58:31 -0600

    New Animated Text

--- src/com/zerulus/states/MenuState.java
+++ src/com/zerulus/states/MenuState.java
@@ -8,10 +8,8 @@ import com.zerulus.main.GamePanel;
 import com.zerulus.util.InputHandler;
 import com.zerulus.util.MouseHandler;
 import com.zerulus.util.Sprite;
+import com.zerulus.util.Vector2f;
 
-import sun.applet.Main;
-
-@SuppressWarnings("unused")
 public class MenuState extends GameState {
 	
 	private final int PLAY = 0;
@@ -30,10 +28,29 @@ public class MenuState extends GameState {
 	private BufferedImage[] img_quit;
 	private BufferedImage[] img_aster;
 	
+	private Vector2f vecAster;
+	private Vector2f vecPlay;
+	private Vector2f vecQuit;
+	private Vector2f vecHighscore;
+	
+	private int sizeAster = 64;
+	private int sizeGen = 32;
+	
+	private boolean animate;
+	private int tick;
+	
+	private final int FADE_OUT = 60;
+	private int alpha;
+	
 	public MenuState(GameStateManager gsm) {
 		super(gsm);
 		
 		Sprite spr_font = new Sprite("/Game/font.png");
+		
+		vecAster = new Vector2f(GamePanel.WIDTH / 2 - (("ASTEROIDS".length() * 64) / 2), 100);
+		vecPlay = new Vector2f(GamePanel.WIDTH / 2 - (("PLAY".length() * 32) / 2), GamePanel.HEIGHT - (GamePanel.HEIGHT / 2));
+		vecHighscore = new Vector2f(GamePanel.WIDTH / 2 - (("HIGHSCORES".length() * 32) / 2), GamePanel.HEIGHT - (GamePanel.HEIGHT / 3) - 32);
+		vecQuit = new Vector2f(GamePanel.WIDTH / 2 - (("QUIT".length() * 32) / 2), GamePanel.HEIGHT - (GamePanel.HEIGHT / 4));
 		
 		img_play = spr_font.getFont("PLAY");
 		img_highscore = spr_font.getFont("HIGHSCORES");
@@ -42,16 +59,52 @@ public class MenuState extends GameState {
 		
 	}
 	
-	public void drawArray(Graphics2D g, BufferedImage[] img, int x, int y, int width, int height, int xOffset, int yOffset) {
+	public void drawArray(Graphics2D g, BufferedImage[] img, Vector2f vec, int width, int height, int xOffset, int yOffset) {
+		float x = vec.x;
+		float y = vec.y;
 		for(int i = 0; i < img.length; i++) {
-			g.drawImage(img[i], x, y, width, height, null);
+			g.drawImage(img[i], (int) x, (int) y, width, height, null);
 			x += xOffset;
 			y += yOffset;
+		}
+	}
+	
+	public void animate() {
+		//Ill come back to this is make it better
+		int size = 100 - (GamePanel.WIDTH / 10);
+		if(animate) {
+			if(tick <= size) {
+				sizeAster += 1;
+				
+				vecAster.x -= 5;
+				vecAster.y += 5;
+				
+			}
+			else if(tick >= size + 30) {
+				if(tick <= size + 45) {
+					vecAster.y -= 5;
+				} else {
+					//System.out.println(vecAster.y += 5);
+					vecAster.y += 40;
+				}
+			}
+			
+			if(vecAster.y >= GamePanel.HEIGHT) {
+				gsm.setState(1, true);
+			}
+			
+			alpha = (int) (255 * (1.0 * tick) / FADE_OUT);
+			if(alpha > 255) alpha = 255;
+			
+			tick++;
 		}
 	}
 
 	@Override
 	public void update() {
+		
+		
+		animate();
 		
 		if(options == PLAY) {
 			choiceX = GamePanel.WIDTH / 2 - (("PLAY".length() * 32) / 2) - 30;
@@ -70,20 +123,31 @@ public class MenuState extends GameState {
 
 	@Override
 	public void render(Graphics2D g) {
-		drawArray(g, img_aster, GamePanel.WIDTH / 2 - (("ASTEROIDS".length() * 64) / 2), 100, 64, 64, 64, 0);
-		drawArray(g, img_play, GamePanel.WIDTH / 2 - (("PLAY".length() * 32) / 2), GamePanel.HEIGHT - (GamePanel.HEIGHT / 2), 32, 32, 32, 0);
-		drawArray(g, img_highscore, GamePanel.WIDTH / 2 - (("HIGHSCORES".length() * 32) / 2), GamePanel.HEIGHT - (GamePanel.HEIGHT / 3) - 32, 32, 32, 32, 0);
-		drawArray(g, img_quit, GamePanel.WIDTH / 2 - (("QUIT".length() * 32) / 2), GamePanel.HEIGHT - (GamePanel.HEIGHT / 4), 32, 32, 32, 0);
 		
-		g.setColor(Color.white);
-		g.fillOval(choiceX, choiceY, choiceWidth, choiceHeight);
+		drawArray(g, img_play, vecPlay, sizeGen, sizeGen, sizeGen, 0);
+		drawArray(g, img_highscore, vecHighscore, sizeGen, sizeGen, sizeGen, 0);
+		drawArray(g, img_quit, vecQuit, sizeGen, sizeGen, sizeGen, 0);
+		
+		g.setColor(new Color(0, 0, 0, alpha));
+		g.fillRect((int) vecPlay.x, (int) vecPlay.y, "Play".length() * sizeGen, "Play".length() * sizeGen);
+		g.fillRect((int) vecHighscore.x, (int) vecHighscore.y, "Highscores".length() * sizeGen, "Highscore".length() * sizeGen);
+		g.fillRect((int) vecQuit.x, (int) vecQuit.y, "Quit".length() * sizeGen, "Quit".length() * sizeGen);
+		
+		
+		drawArray(g, img_aster, vecAster, sizeAster, sizeAster, sizeAster, 0);
+		
+		if(!animate) {
+			g.setColor(Color.white);
+			g.fillOval(choiceX, choiceY, choiceWidth, choiceHeight);
+		}
+		
 	}
 
 	@Override
 	public void input(InputHandler keys, MouseHandler mouse) {
 		if(keys.enter.down) {
 			if(options == PLAY) {
-				gsm.setState(GameStateManager.PLAY, true);
+				animate = true;
 			}
 			if(options == QUIT) {
 				System.exit(0);
