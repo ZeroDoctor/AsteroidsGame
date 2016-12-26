2fc5de3d262a9cfb18911c838c9626f94f4a6dee
Zerulus <danielrcastro10@gmail.com>
2016-12-26 11:00:09 -0600

    still needs to work with collisions

--- src/com/zerulus/entity/Asteroids.java
+++ src/com/zerulus/entity/Asteroids.java
@@ -1,5 +1,41 @@
 package com.zerulus.entity;
 
-public class Mob {
+import java.awt.Graphics2D;
+
+import com.zerulus.util.Vector2f;
+
+public class Asteroids {
+	
+	private int size;
+	private double angle;
+	private double speed;
+	private Vector2f vec;
+	
+	public Asteroids(int id, Vector2f vec, double angle) {
+		this.vec = vec;
+		this.angle = angle;
+		
+		if(id == 1) {
+			size = 64;
+			speed = 2;
+		}
+		else if(id == 2) {
+			size = 32;
+			speed = 4;
+		}
+		else {
+			size = 16;
+			speed = 8;
+		}
+	}
+	
+	public void update() {
+		vec.x += Math.cos(Math.toRadians(angle)) * speed;
+		vec.y += Math.sin(Math.toRadians(angle)) * speed;
+	}
+	
+	public void render(Graphics2D g) {
+		g.fillOval((int) vec.x, (int) vec.y, size, size);
+	}
 	
 }
