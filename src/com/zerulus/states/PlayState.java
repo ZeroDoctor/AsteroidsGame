8ebeae5614b43a6caceb8cdd7d8040fbf92731e7
Zerulus <danielrcastro10@gmail.com>
2016-12-27 00:27:18 -0600

    Testing collisions

--- src/com/zerulus/states/PlayState.java
+++ src/com/zerulus/states/PlayState.java
@@ -1,32 +1,44 @@
 package com.zerulus.states;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 
+import com.zerulus.entity.Asteroids;
 import com.zerulus.entity.Player;
+import com.zerulus.util.AABB;
 import com.zerulus.util.InputHandler;
 import com.zerulus.util.MouseHandler;
 
 public class PlayState extends GameState {
 	
 	private Player p;
+	private Asteroids a;
+	private AABB check;
 	private int mouseX;
 	private int mouseY;
 	
 	
 	public PlayState(GameStateManager gsm) {
 		super(gsm);
+		check = new AABB();
 		p = new Player();
+		a = new Asteroids();
 	}
 
 	@Override
 	public void update() {
 		p.update(mouseX, mouseY);
+		a.update();
 		
+		if(check.colCircletoBox(p.playerBounds, a.asterBounds)) {
+			System.out.println("OUCH!!!");
+		}
 	}
 
 	@Override
 	public void render(Graphics2D g) {
 		p.render(g);
+		a.render(g);
 	}
 
 	@Override
