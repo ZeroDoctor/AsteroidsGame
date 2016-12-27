97765f0eb012277a51803fa25f16fdf969c9e282
Zerulus <danielrcastro10@gmail.com>
2016-12-27 00:28:14 -0600

    added Bounding Circle

--- src/com/zerulus/entity/Asteroids.java
+++ src/com/zerulus/entity/Asteroids.java
@@ -1,7 +1,10 @@
 package com.zerulus.entity;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 
+import com.zerulus.main.GamePanel;
+import com.zerulus.util.AABB;
 import com.zerulus.util.Vector2f;
 
 public class Asteroids {
@@ -10,6 +13,17 @@ public class Asteroids {
 	private double angle;
 	private double speed;
 	private Vector2f vec;
+	
+	public AABB asterBounds;
+	
+	public Asteroids() {
+		vec = new Vector2f(GamePanel.WIDTH / 2, GamePanel.HEIGHT / 2);
+		size = 64;
+		speed = 0;
+		angle = 0;
+		
+		asterBounds = new AABB(vec, size);
+	}
 	
 	public Asteroids(int id, Vector2f vec, double angle) {
 		this.vec = vec;
@@ -32,9 +46,11 @@ public class Asteroids {
 	public void update() {
 		vec.x += Math.cos(Math.toRadians(angle)) * speed;
 		vec.y += Math.sin(Math.toRadians(angle)) * speed;
+		asterBounds.setCircle(vec, size);
 	}
 	
 	public void render(Graphics2D g) {
+		g.setColor(Color.white);
 		g.fillOval((int) vec.x, (int) vec.y, size, size);
 	}
 	
