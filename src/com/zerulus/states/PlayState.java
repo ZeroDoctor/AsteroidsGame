b0a3ea4ad1b327f9f1fbd2a6d3605a8ea74a223c
zerulus <zerulus@mrzerulus>
2016-12-30 00:04:47 -0600

    added collision response and changed the render order of bullet and player

--- src/com/zerulus/states/PlayState.java
+++ src/com/zerulus/states/PlayState.java
@@ -1,44 +1,118 @@
 package com.zerulus.states;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
+import java.util.ArrayList;
+import java.util.Random;
 
 import com.zerulus.entity.Asteroids;
+import com.zerulus.entity.Bullet;
 import com.zerulus.entity.Player;
+import com.zerulus.main.GamePanel;
 import com.zerulus.util.AABB;
 import com.zerulus.util.InputHandler;
 import com.zerulus.util.MouseHandler;
+import com.zerulus.util.Vector2f;
 
 public class PlayState extends GameState {
 	
 	private Player p;
-	private Asteroids a;
+	private Random rand;
+	private ArrayList<Bullet> bullets;
+	private ArrayList<Asteroids> asteroids;
 	private AABB check;
 	private int mouseX;
 	private int mouseY;
 	
+	private int tick;
+	private int create = 15;
+	private boolean canCreate = true;
 	
 	public PlayState(GameStateManager gsm) {
 		super(gsm);
 		check = new AABB();
 		p = new Player();
-		a = new Asteroids();
+		asteroids = new ArrayList<Asteroids>();
+		bullets = new ArrayList<Bullet>();
+		rand = new Random();
+		
+		
+		for(int i = 0; i < 3; i++) {
+			float x = (float) rand.nextInt(GamePanel.WIDTH);
+			float y = (float) rand.nextInt(GamePanel.HEIGHT);
+			asteroids.add(new Asteroids(1, new Vector2f(x, y), rand.nextInt(360)));
+		}
+	}
+	
+	public void createBullet() {
+		if(canCreate) {
+			canCreate = false;
+			bullets.add(new Bullet(new Vector2f(p.vec.x + (p.size / 2), p.vec.y + (p.size / 2)), new Vector2f(mouseX, mouseY), p.getDx(), p.getDy()));
+		} else {
+			if(tick >= create) {
+				canCreate = true;
+				tick = 0;
+			}
+		}
 	}
 
 	@Override
 	public void update() {
 		p.update(mouseX, mouseY);
-		a.update();
 		
-		if(check.colCircletoBox(p.playerBounds, a.asterBounds)) {
-			System.out.println("OUCH!!!");
+		for(int i = 0; i < asteroids.size(); i++) {
+			asteroids.get(i).update();
+			if(check.colCircletoBox(p.playerBounds, asteroids.get(i).asterBounds)) {
+				System.out.println("OUCH!!!");
+			}
 		}
+		
+		for(int i = 0; i < bullets.size(); i++) {
+			if(bullets.get(i).update()) {
+				bullets.remove(i);
+			}
+		}
+		
+		
+		/**
+		 * I didn't think about having to worry about checking 
+		 * collisions from two different ArrayList. We should
+		 * think about coding AABB class a little bit differently.
+		 * However, I'm just going with the flow right now.
+		 * 
+		 * */
+		for(int i = 0; i < bullets.size(); i++) {
+			for(int j = 0; j < asteroids.size(); j++) {
+				if(check.colCircletoBox(bullets.get(i).bulletBounds, asteroids.get(j).asterBounds)){
+					int id = asteroids.get(j).getID();
+					Vector2f pos = asteroids.get(j).getPos();
+					asteroids.remove(j);
+					//still working on this
+					if(id == 1) {
+						asteroids.add(new Asteroids(2, pos, rand.nextInt(360)));
+						asteroids.add(new Asteroids(2, pos, rand.nextInt(360)));
+					}
+					bullets.remove(i);
+					break;
+				}
+			}
+		}
+		
+		tick++;
+		
 	}
 
 	@Override
 	public void render(Graphics2D g) {
+		
+		for(int i = 0; i < bullets.size(); i++) {
+			bullets.get(i).render(g);
+		}
+		
 		p.render(g);
-		a.render(g);
+		
+		for(int i = 0; i < asteroids.size(); i++) {
+			asteroids.get(i).render(g);
+		}
 	}
 
 	@Override
@@ -69,6 +143,11 @@ public class PlayState extends GameState {
 		} else {
 			p.right = false;
 		}
+		
+		if(mouse.getButton() == 1) {
+			createBullet();
+		}
+		
 	}
 
 }
