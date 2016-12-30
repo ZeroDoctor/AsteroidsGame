336fb7627b345a69b134c885ef10e96ed778aae5
zerulus <zerulus@mrzerulus>
2016-12-30 00:02:56 -0600

    added rotation still has some bugs

--- src/com/zerulus/entity/Bullet.java
+++ src/com/zerulus/entity/Bullet.java
@@ -1,40 +1,93 @@
 package com.zerulus.entity;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.geom.Ellipse2D;
+import java.awt.Rectangle;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
 
-import com.zerulus.util.Vector2f;
+import com.zerulus.main.GamePanel;
+import com.zerulus.util.AABB;
+import com.zerulus.util.Animation;
+import com.zerulus.util.Sprite;
+import com.zerulus.util.Vector2f; 
 
-public class Bullet {
+public class Bullet extends Rectangle {
+	private static final long serialVersionUID = 1L;
 	
 	private Vector2f vec;
-	private Vector2f vec2;
-	private int size = 16;
-	private int life = 300;
+	private float x2, y2;
+	private Vector2f vec3;
+	private float pxSpeed;
+	private float pySpeed;
 	
-	public Bullet(Vector2f vec, Vector2f vec2) {
+	private int w = 6;
+	private int h = 16;
+	//FYI 60 ticks is a second
+	private float life = 30;
+	private int tick;
+	
+	public AABB bulletBounds;
+	
+	private Sprite spr_bullet = new Sprite("/Game/bullet.png");
+	private BufferedImage[] img_bullet = {spr_bullet.getSprite()};
+	private Animation rotate = new Animation(img_bullet, 10);
+	private AffineTransformOp op;
+
+	public Bullet(Vector2f vec, Vector2f vec2, float pxSpeed, float pySpeed) {
 		this.vec = vec;
-		this.vec2 = vec2;
+		x2 = vec.x;
+		y2 = vec.y;
+		this.vec3 = vec2;
+		
+		this.pxSpeed = Math.abs(pxSpeed);
+		this.pySpeed = Math.abs(pySpeed);
+		
+		setBounds((int) vec.x, (int) vec.y, w, h);
+		bulletBounds = new AABB(vec, w, h);
 	}
 	
 	//if true than remove Bullet
 	public boolean update() {
-		if(vec.x > vec2.x + life || vec.y > vec2.y +life || vec.x < vec2.x - life || vec.y < vec2.y - life)
+		if(tick >= life)
 			return true;
 		
-		double dx = vec.x - vec2.x;
-		double dy = vec.y - vec2.y;
+		double dx = vec3.x - x2;
+		double dy = vec3.y - y2;
 		double rad = Math.atan2(dy, dx);
+		//Still working on bullet rotations
+		op = rotate.rotate(rad + 3.14 / 2, w, h);
 		
-		vec.x += (Math.cos(rad) * 20);
-		vec.y += (Math.sin(rad) * 20);
+		vec.x += (Math.cos(rad) * (20 + pxSpeed));
+		vec.y += (Math.sin(rad) * (20 + pySpeed));
+		
+		if(vec.x > GamePanel.WIDTH) {
+			vec.x = -w;
+		}
+		if(vec.x < -w) {
+			vec.x = GamePanel.WIDTH;
+		}
+		if(vec.y > GamePanel.HEIGHT + h) {
+			vec.y = -h;
+		}
+		if(vec.y < -h) {
+			vec.y = GamePanel.HEIGHT + h;
+		}
+		
+		bulletBounds.setBox(vec, w, h);
+		setBounds((int) vec.x, (int) vec.y, w, h);
+		
+		tick++;
 		return false;
 	}
 	
+	
 	public void render(Graphics2D g) {
-		g.setColor(Color.white);
-        g.fill(new Ellipse2D.Float(vec.x, vec.y, size, size));
+		//if(op == null) System.out.println("OP is null");
+		
+		if(img_bullet[0] == null || op == null)
+			g.drawImage(img_bullet[0], (int) vec.x, (int) vec.y, w, h, null);
+		else
+			g.drawImage(op.filter(img_bullet[0], null), (int) vec.x, (int) vec.y, null);
 	}
 	
 }
