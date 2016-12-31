8cd3ca42effbbfb3fb44fe2a587c3e67dc5d5518
Zerulus <danielrcastro10@gmail.com>
2016-12-30 22:16:09 -0600

    added comments and added drawArray methods

--- src/com/zerulus/util/Sprite.java
+++ src/com/zerulus/util/Sprite.java
@@ -1,9 +1,11 @@
 //code used from http://gamedev.stackexchange.com/questions/53705/how-can-i-make-a-sprite-sheet-based-animation-system
 package com.zerulus.util;
 
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 
@@ -37,6 +39,20 @@ public class Sprite {
 		return sprite;
 	}
 	
+	/*######################## getSprites ######################*/
+	public BufferedImage getSprite() {
+		return spriteSheet;
+	}
+	
+	public BufferedImage getSprite(int xGrid, int yGrid){
+		if (spriteSheet == null) {
+			spriteSheet = loadSprite(Main.class.getResourceAsStream("AnimationSpriteSheet"));
+		}
+		
+		return spriteSheet.getSubimage(xGrid * w, yGrid * h, w, h);
+	}
+	
+	/*######################## getFonts ########################*/
 	public BufferedImage[] getFont(String word) {
 		
 		BufferedImage[] img_word = new BufferedImage[30];
@@ -50,16 +66,41 @@ public class Sprite {
 		
 	}
 	
-	public BufferedImage getSprite() {
-		return spriteSheet;
-	}
-	
-	public BufferedImage getSprite(int xGrid, int yGrid){
-		if (spriteSheet == null) {
-			spriteSheet = loadSprite(Main.class.getResourceAsStream("AnimationSpriteSheet"));
+	public ArrayList<BufferedImage> getFont(Integer k){
+		
+		ArrayList<BufferedImage> num = new ArrayList<BufferedImage>();
+		
+		int y = 4;
+		String word = k.toString();
+		for(int i = 0; i < k.toString().length(); i++) {
+			int x = word.charAt(i) - 48;
+			if(x > 3) {
+				y = 5;
+			}
+			num.add(spriteSheet.getSubimage(((2 + x) % 6) * 48, y * 48, 48, 48));
 		}
 		
-		return spriteSheet.getSubimage(xGrid * w, yGrid * h, w, h);
+		return num;
+	}
+	
+	public static void drawArray(Graphics2D g, BufferedImage[] img, Vector2f vec, int width, int height, int xOffset, int yOffset) {
+		float x = vec.x;
+		float y = vec.y;
+		for(int i = 0; i < img.length; i++) {
+			g.drawImage(img[i], (int) x, (int) y, width, height, null);
+			x += xOffset;
+			y += yOffset;
+		}
+	}
+	
+	public static void drawArray(Graphics2D g, ArrayList<BufferedImage> img, Vector2f vec, int width, int height, int xOffset, int yOffset) {
+		float x = vec.x;
+		float y = vec.y;
+		for(int i = 0; i < img.size(); i++) {
+			g.drawImage(img.get(i), (int) x, (int) y, width, height, null);
+			x += xOffset;
+			y += yOffset;
+		}
 	}
 
 }
